f2402527cb0fc727644d0346cfa295fb8a07aa46
andyque <guanghui8827@126.com>
2014-09-28 16:04:09 +0800

    finish iosmac compile scripts

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-source android/libpng.sh
+# we must specify the ndk root first before running this scripts
+# export NDK_ROOT=/Users/guanghui/AndroidDev/android-ndk-r9d/
+# export PATH=$NDK_ROOT:$PATH
+# export ANDROID_SDK_ROOT=/Users/guanghui/AndroidDev/adt-bundle-mac-x86_64-20130522/sdk/
+source ~/.bash_profile
+export PATH=$ANDROID_SDK_ROOT:$PATH
+export PATH=$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH
+export ANT_ROOT=/usr/local/bin
+export NDK_MODULE_PATH=`pwd`
+echo $NDK_MODULE_PATH
+
+cd android/libpng
+
+NDK-build
 
 
--- build_iosmac.sh
+++ build_iosmac.sh
@@ -5,10 +5,22 @@ cd ios_mac/
 sh build_zlib.sh
 sh build_libpng.sh
 
-cd ..
 
-mkdir -p prebuilt/ios
+mkdir -p prebuilt/include/
+
+echo "move ios static library to prebuilt/ios"
+mkdir  prebuilt/ios
 mv libpng/prebuilt/ios/libpng.a ./prebuilt/ios
 
-mkdir -p prebuilt/mac
+echo "move ios include header files to prebuilt/include/ios"
+mv libpng/prebuilt/include/ios ./prebuilt/include/
+
+echo "move static library to prebuilt/mac"
+mkdir  prebuilt/mac
 mv libpng/prebuilt/mac/libpng.a ./prebuilt/mac
+
+echo "move mac include header files to prebuilt/include/mac"
+mv libpng/prebuilt/include/mac ./prebuilt/include/
+
+echo "remove zlib temp files"
+rm -rf zlib
