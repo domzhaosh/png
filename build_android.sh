298738ca11d8cb4c44d741850058251813c5c9b5
andyque <guanghui8827@126.com>
2014-09-29 15:52:18 +0800

    finish armeabi-v7a

--- build_android.sh
+++ build_android.sh
@@ -12,24 +12,27 @@ sh $ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-$AND
 export PATH=`pwd`/android-toolchain/bin:$PATH
 echo $PATH
 
-# build for armeabi-v7a
 CC=arm-linux-androideabi-gcc
 CXX=arm-linux-androideabi-g++
 outputpath=`pwd`/androidoutput
 echo $outputpath
 android_system_root=`pwd`/android-toolchain/sysroot
 echo "sysroot is $android_system_root"
-CFLAGS="-isysroot $android_system_root -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
-LDFLAGS="-isysroot $android_system_root -march=armv7-a -Wl,--fix-cortex-a8"
+
+
+# build for armeabi-v7a
+CFLAGS="--sysroot=$android_system_root -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
+LDFLAGS="--sysroot=$android_system_root -march=armv7-a -Wl,--fix-cortex-a8"
 
 cd libpng/
-./configure --prefix=$outputpath --enable-static=yes --host=arm-linux-androideabi --with-sysroot=$android_system_root
+./configure --prefix=$outputpath --enable-static=yes --host=arm-linux-androideabi
 make
 make install
 
 #strip and copy
-arm-linux-androideabi-strip $outputpath/lib/libpng16.a
+cd ..
+arm-linux-androideabi-strip --strip-unneeded $outputpath/lib/libpng16.a
 mkdir -p prebuilt/android/armeabi-v7a
-cp $outputpat/lib/libpng16.a prebuilt/android/armeabi-v7a/
+cp $outputpath/lib/libpng16.a prebuilt/android/armeabi-v7a/libpng.a
 
 
